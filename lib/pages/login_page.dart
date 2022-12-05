399dbf9939ece568a0d85538e7108ef001806dad
“Ridwan <ridwanmiharbi@gmail.com>
2022-12-06 06:15:55 +0700

    Day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,9 +5,52 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text('Login Page'),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/login.png',
+            fit: BoxFit.scaleDown,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Welcome',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 13.0,
+              horizontal: 30.0,
+            ),
+            child: Column(
+              children: [
+                const TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter Username',
+                    labelText: 'Username',
+                  ),
+                ),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    labelText: 'Password',
+                  ),
+                ),
+                ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
